3dfcf774a7ed629463d0fa4358f2123045b35056
stak21 <stakashima21@gmail.com>
2018-06-02 19:13:40 -0700

    Finished leetCode#203, Things to work on, remember when comparing nodes compare the value.

--- leetCode#203.js
+++ leetCode#203.js
@@ -10,46 +10,36 @@
  * @param {number} val
  * @return {ListNode}
  */
-//when you come back, the current problem is [1,1,1] 1 You can't read val of null because you are skipping all of thme.
 var removeElements = function(head, val) {
-  var currentNodePTR = head
-removeNode = () => {
-     console.log(currentNodePTR.val);
-  var prevNodePTR = currentNodePTR;
-    currentNodePTR = currentNodePTR.next;
-    if(currentNodePTR === null) {
-        prevNodePTR.next = currentNodePTR;
-        return head;
+  currentNodePTR = head;
+  //removeNode function
+  let removeNode = () => {
+    let temp = currentNodePTR.next;
+    currentNodePTR.next = temp.next;
+    temp.next = null;
+  }
+  //pre-check
+  if (head === null) {
+    return head;
+  }
+  else if (val === null) {
+    return head;
+  }
+  //main loop
+  while (currentNodePTR !== null) {
+    if (head.val === val) {
+      head = head.next;
+      currentNodePTR = head;
     }
-    prevNodePTR.next = currentNodePTR.next;
-    console.log(currentNodePTR.val);
-}
-// nodeValue = () => {
-//     while(currentNodePTR !== null) {
-//         console.log(currentNodePTR.val);
-//         currentNodePTR = currentNodePTR.next;
-//     }
-// }
-//iterate though head until null
-  if(currentNodePTR === null) {
+    else if (currentNodePTR.next === null) {
       return head;
-  }
-  if(currentNodePTR.next === null) {
-    return head;
-}
-while(currentNodePTR !== null ) {
-    if(currentNodePTR.val === val) {  //checks if head is EQ to val
-      head = currentNodePTR.next;
     }
-  if(currentNodePTR.next.val === val) { // if compared values are equal then remove the element
-console.log("test");
-    removeNode();
+    else if (currentNodePTR.next.val === val) {
+        removeNode();
+    }
+    else {
+      currentNodePTR = currentNodePTR.next;
+    }
   }
-  //check to see if current is set to null
-  if(currentNodePTR === null) {
     return head;
-  }
-  currentNodePTR = currentNodePTR.next;
-}
-  return head;
 };
