18534980629a07fb635509d7846521f95b26a1e0
stak21 <stakashima21@gmail.com>
2018-08-06 23:23:23 -0700

    started and finished leetCode#728

---
+++ leetCode/leetCode#728.js
@@ -0,0 +1,24 @@
+var divMod = (left) => {
+  let temp = left;
+  const divTo = [];
+  while (temp >= 1) {
+    divTo.push(parseInt(temp, 0) % 10);
+    temp /= 10;
+  }
+  return divTo;
+};
+var selfDividingNumbers = function (left, right) {
+  const final = [];
+  for (left; left <= right; left += 1) {
+    let num = left;
+    const divTo = divMod(num);
+    divTo.sort();
+    if (divTo[0] !== 0) {
+      if (divTo.every(item => num % item === 0)) {
+        final.push(num);
+      }
+    }
+  }
+  return final;
+};
+selfDividingNumbers(1, 15);
