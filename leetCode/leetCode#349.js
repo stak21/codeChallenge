0bb275a806ed0f30079d4203f2e4b9e9f9e55443
stak21 <stakashima21@gmail.com>
2018-08-15 22:10:17 -0700

    started leetCode#349.js

---
+++ leetCode/leetCode#349.js
@@ -0,0 +1,19 @@
+var intersection = function(nums1, nums2) {
+  const result =  nums1.filter((item) => {
+       if (nums2.includes(item) === true) {
+           while (nums2.includes(item)) {
+                  let index = nums2.findIndex(element => element === item);
+                  nums2.splice(index, 1);
+                  }
+                  if (item === 0) {
+                    item = '0';
+                  }
+           return item;
+       }
+   });
+   return result;
+};
+const nums1 = [8,0,3,1]
+
+const nums2 = [0,0,1]
+intersection(nums1, nums2);  
